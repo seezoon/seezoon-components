ebed174e218243ecc7ea1125ea3cd9e555d5da5d
dfenghuang <dfenghuang@tencent.com>
2022-02-14 00:50:47 +0800

    refactor

---
+++ seezoon-application-archetype/src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/infrastructure/exception/GrpcExceptionAdvice.java
@@ -0,0 +1,25 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.infrastructure.exception;
+
+
+import com.seezoon.core.concept.infrastructure.exception.BaseException;
+import com.seezoon.grpc.support.GrpcExceptionTranslator;
+import com.seezoon.web.api.Result;
+import io.grpc.StatusRuntimeException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * grpc rpc 远程调用时候无法通过grpc client拦截器完成统一异常处理
+ */
+@RestControllerAdvice
+public class GrpcExceptionAdvice {
+
+    @ExceptionHandler({StatusRuntimeException.class})
+    public Result statusRuntimeException(StatusRuntimeException e) {
+        BaseException baseException = GrpcExceptionTranslator.translate(e);
+        return Result.error(baseException.getCode(), baseException.getMessage());
+    }
+}
