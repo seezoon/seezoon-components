e26a665a9d79ab1afef0b7fb3b2d191f41c63f6d
dfenghuang <dfenghuang@tencent.com>
2022-02-25 11:39:21 +0800

    add profile cli

--- seezoon-application/seezoon-application-server/build/assembly/conf/setenv.sh
+++ seezoon-application/seezoon-application-server/build/assembly/conf/setenv.sh
@@ -15,7 +15,6 @@ fi
 JVM_ARGS="-XX:AutoBoxCacheMax=2000 -XX:+UseStringDeduplication -XX:+UseG1GC -verbose:gc -Xlog:gc*,safepoint:${LOG_PATH}/gc.log:time,uptime:filecount=1,filesize=10M -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:HeapDumpPath=${LOG_PATH}/dump -XX:ErrorFile=${LOG_PATH}/hs_err_%p.log"
 #优雅关机等待时间
 SHUTDOWN_SECONDS=30
-# 如果是多环境配置文件内置，则添加 --spring.profiles.active=${env}
-SERVER_OTPS="--server.tomcat.basedir=. --server.tomcat.connection-timeout=5s --server.tomcat.max-connections=2000 --server.tomcat.accept-count=200 --server.tomcat.threads.min-spare=10 --server.tomcat.threads.max=500 --server.shutdown=graceful --spring.lifecycle.timeout-per-shutdown-phase=${SHUTDOWN_SECONDS}s"
+SERVER_OTPS="--spring.profiles.active=cli --server.tomcat.basedir=. --server.tomcat.connection-timeout=5s --server.tomcat.max-connections=2000 --server.tomcat.accept-count=200 --server.tomcat.threads.min-spare=10 --server.tomcat.threads.max=500 --server.shutdown=graceful --spring.lifecycle.timeout-per-shutdown-phase=${SHUTDOWN_SECONDS}s"
 # export for java properties
 export LOG_PATH
