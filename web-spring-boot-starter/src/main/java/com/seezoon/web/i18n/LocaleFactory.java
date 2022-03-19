4a2dc8c8eb784ef0a45b8e18c34139175c460052
dfenghuang <dfenghuang@tencent.com>
2022-03-19 20:42:52 +0800

    fix(i18n): add i18n facotry

--- web-spring-boot-starter/src/main/java/com/seezoon/web/i18n/LocaleFactory.java
+++ web-spring-boot-starter/src/main/java/com/seezoon/web/i18n/LocaleFactory.java
@@ -3,6 +3,7 @@ package com.seezoon.web.i18n;
 import java.util.Locale;
 import org.apache.commons.lang3.LocaleUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 public class LocaleFactory {
 
@@ -12,5 +13,9 @@ public class LocaleFactory {
 
     public static Locale create(String locale) {
         return StringUtils.isEmpty(locale) ? getDefaultLocale() : LocaleUtils.toLocale(locale);
+    }
+
+    public static Locale getCurrent() {
+        return LocaleContextHolder.getLocale();
     }
 }
